e11cab6e0d38d4542d65f4a3a9187f99f6f55768
Kibeom Jeon <kibeom1145@gmail.com>
2023-12-26 20:49:30 +0900

    .

--- server/server.js
+++ server/server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
